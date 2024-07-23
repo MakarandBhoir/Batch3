e49565325449e60c8b1b11e9f04155cc33341fbe
makarand.bhoir <makarand.bhoir@live.com>
2024-07-23 20:54:47 +0530

    stream api-day3

---
+++ 09-StreamApiFundamentals/src/stream/pack08/Test.java
@@ -0,0 +1,64 @@
+package stream.pack08;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class Test {
+	public static void main(String[] args) {
+		Employee e1 = new Employee(101, "John", 10000);
+		Employee e2 = new Employee(102, "Smith", 20000);
+		Employee e3 = new Employee(103, "Peter", 30000);
+		Employee e4 = new Employee(104, "David", 40000);
+		Employee e5 = new Employee(105, "Sam", 50000);
+		Employee e6 = new Employee(106, "John", 60000);
+		Employee e7 = new Employee(107, "Smith", 70000);
+		Employee e8 = new Employee(108, "Peter", 80000);
+		Employee e9 = new Employee(109, "David", 90000);
+		Employee e10 = new Employee(110, "Sam", 100000);
+		
+		List<Employee> list = new ArrayList<>();
+		list.add(e1);
+		list.add(e2);
+		list.add(e3);
+		list.add(e4);
+		list.add(e5);
+		list.add(e6);
+		list.add(e7);
+	    list.add(e8);
+	    list.add(e9);
+	    list.add(e10);
+	    
+	    Stream<Employee> stm = list.stream();
+	    
+	    Function<Employee, String> function = (Employee emp) -> {
+	    	return emp.getEmployeeName();
+	    };
+
+	    Function<Employee, String> function2 = emp -> emp.getEmployeeName();
+	   
+	    
+	    Stream<String> stm2 = stm.map(function2);
+	    stm2.forEach( e -> System.out.println(e) );
+	    
+	    System.out.println("---------------------------------------------------------");
+	    
+	    list.stream().mapToDouble(emp -> emp.getEmployeeSalary()).forEach(e -> System.out.println(e));
+	    
+	    
+	    System.out.println("---------------------------------------------------------");
+	    
+	    double max =  list.stream().mapToDouble(emp -> emp.getEmployeeSalary()).max().getAsDouble();
+	    double min =  list.stream().mapToDouble(emp -> emp.getEmployeeSalary()).min().getAsDouble();
+	    double sum =  list.stream().mapToDouble(emp -> emp.getEmployeeSalary()).sum();
+	    double avg =  list.stream().mapToDouble(emp -> emp.getEmployeeSalary()).average().getAsDouble();
+	    
+	    System.out.println("Max: " + max);
+	    System.out.println("Min: " + min);
+	    System.out.println("Sum: " + sum);
+	    System.out.println("Avg: " + avg);
+	    
+	}
+}
